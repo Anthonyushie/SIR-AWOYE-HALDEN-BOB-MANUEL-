22031ad3f8b263f2d72cb5ba417ade31f03eed28
Anthonyushie <anthonytwan75official@gmail.com>
2024-08-05 19:27:59 +0100

    Mobile

--- src/components/PDFViewer.jsx
+++ src/components/PDFViewer.jsx
@@ -2,27 +2,28 @@ import React from 'react';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import pdfFile from '../assets/bob-manuel-prog.pdf';  // Adjust the path as necessary
-import '../App.css'; // Import global CSS file if needed
+import pdfFile from '../assets/bob-manuel-prog.pdf';
 
 function PDFViewer() {
+  // Define different scales for different screen sizes
+  const defaultScale = window.innerWidth < 768 ? 0.6 : 1.7; // 1.0x zoom for mobile, 2.0x for desktop
+
   return (
     <div className="flex flex-col items-center p-2 md:p-8 lg:p-12">
       <h1 className="text-xl md:text-3xl lg:text-4xl font-bold mb-4">PDF Viewer</h1>
-      <div className="w-full max-w-full md:max-w-5xl"> {/* Increased max width for larger view */}
+      <div className="w-full max-w-full md:max-w-5xl">
         <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-        <Viewer
-        fileUrl={pdfFile}
-        theme="dark"
-        defaultScale={1.7} // Keep the zoom level at 2.0x
-        style={{
-          height: '100vh', // Increase height to fill the viewport
-          width: '100%', // Use full width
-          margin: '0 auto', // Center the PDF
-          backgroundColor: '#fff', // Set background color to white
-        }}
-      />
-
+          <Viewer
+            fileUrl={pdfFile}
+            theme="dark"
+            defaultScale={defaultScale}
+            style={{
+              height: '100vh',
+              width: '100%',
+              margin: '0 auto',
+              backgroundColor: '#fff',
+            }}
+          />
         </Worker>
       </div>
     </div>
