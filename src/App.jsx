fb36c90d281ccba649aafb6ffe37c9a7b8916a83
Anthonyushie <anthonytwan75official@gmail.com>
2024-08-05 09:22:23 +0100

    Initial commit✅

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PDFViewer from './components/PDFViewer';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <PDFViewer />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
